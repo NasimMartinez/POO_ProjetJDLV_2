0aa01f853248a631fba851d6d7e68521cbf6e907
TasGlitch <taharabbesn4@gmail.com>
2025-12-04 11:29:12 +0100

    attempts at fixing

--- POO_ProjetJDLV_2/src/main.cpp
+++ POO_ProjetJDLV_2/src/main.cpp
@@ -1,9 +1,12 @@
+#include "../lib/common/libs.h"
 #include "../lib/Manager/manager.h"
 #include "../lib/testBlocK/testBlock.cpp"
 
 int main() {
+    /*
     Manager manager;
     manager.run();
+    */
     std::cout << "=== Lancement des tests unitaires ===" << std::endl;
 
     testBlockStable();
